e47a8686f5869c88c0d7b1d732823df15fa922ed
saima-salar <157168170+saima-salar@users.noreply.github.com>
2025-03-11 02:17:49 +0500

    app.py
    edit

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ st.markdown(""""
 <style>
     .main {text-align: center;}
     .stTextInput {width:60% !important; margin:auto;}
-    .stButton button {width:50%, background-color: #f63366 color: white; font-size: 18px;}
-    .stButton button:hover {background-color: #f33661;}
+    .stButton button {width:50%, background-color:blue color: white; font-size: 18px;}
+    .stButton button:hover {background-color: red;}
 </style>
 """, unsafe_allow_html=True)
 
